48b03aa895803d20ce523ed2e3a4453ea5da1a56
adithyan pr <108983785+Aathi-pr@users.noreply.github.com>
2024-06-03 17:23:52 +0530

    all fles are kida ready

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    var navLinks = document.querySelector('.nvLinks ul');
+    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+    
+ });
